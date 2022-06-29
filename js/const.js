bda751750100df90366e5edf052ddff8966df8a4
bp-ooya <bp-ooya@kdl.co.jp>
2022-06-29 17:01:41 +0900

    テスト用

--- js/const.js
+++ js/const.js
@@ -1,4 +1,4 @@
 const api_uri = 'https://logitem-dev.herokuapp.com/';
-const client_id = '3074457361560843499';		// 開発用
-//const client_id = '3458764516006800161';		// テスト用
+//const client_id = '3074457361560843499';		// 開発用
+const client_id = '3458764516006800161';		// テスト用
 const maxFont=57;
